4943b10d9e124af3d8e93ce06708a22e8d1f0a85
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2025-06-17 13:31:42 +0530

    Update Task1.py

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -1,1 +1,12 @@
+first_number = int(input("Enter the First Number: "))
+second_number = int(input("Enter the Second Number: "))
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / \
+    second_number if second_number != 0 else "Undefined (cannot divide by zero)"
 
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
