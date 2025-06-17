fe65f01ea1a9b8ea09e61fa9772bb3878166be6c
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2025-06-17 14:29:47 +0530

    Update Task1.py

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -1,5 +1,5 @@
-first_number = int(input("Enter the First Number: "))
-second_number = int(input("Enter the Second Number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 addition = first_number + second_number
 subtraction = first_number - second_number
 multiplication = first_number * second_number
